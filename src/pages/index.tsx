9f3e12ea5ea9d896fdd04c8daa0a0d5db9b69fdd
Arthur Amaral <ruhtraamaral@hotmail.com>
2023-11-12 17:07:57 -0300

    Change icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import Head from "next/head";
-import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Inter } from "@next/font/google";
+import Head from "next/head";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <Image
+          src="/Site logo.png"
+          alt="Logo"
+          width={500}
+          height={500}
+          className="logo-bg"
+        />
         <span>Under</span>
         <span>Construction</span>
       </main>
